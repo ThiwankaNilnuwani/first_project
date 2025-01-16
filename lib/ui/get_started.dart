cfd6bdb54358cdd414d1e51f5bb2b04f69b64f3e
ThiwankaNilnuwani <thiwankanilnuwaniwijerathne001@gmail.com>
2025-01-16 22:44:10 +0530

    initial commit

---
+++ lib/ui/get_started.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class GetStarted extends StatelessWidget {
+  const GetStarted({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        color: Colors.red,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center ,
+            children: [],
+          ),
+        ) ,
+      ),
+
+
+      );
+
+  }
+}
