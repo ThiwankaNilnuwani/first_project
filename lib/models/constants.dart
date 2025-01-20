90feae5fa204c94429c26ab2af3910f74477e78e
ThiwankaNilnuwani <thiwankanilnuwaniwijerathne001@gmail.com>
2025-01-20 13:44:48 +0530

    Home page created

---
+++ lib/models/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  final Color primaryColor = const Color.fromARGB(255, 79, 105, 161);
+  final Color secondaryColor = const Color.fromARGB(255, 52, 97, 188);
+}
--- lib/ui/get_started.dart
+++ lib/ui/get_started.dart
@@ -1,3 +1,5 @@
+import 'package:first_project/models/constants.dart';
+import 'package:first_project/ui/welcome.dart';
 import 'package:flutter/material.dart';
 
 class GetStarted extends StatelessWidget {
@@ -5,26 +7,50 @@ class GetStarted extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Constants myConstants = Constants();
     Size size = MediaQuery.of(context).size;
-
-    //start UI
 
     return Scaffold(
       body: Container(
         width: size.width,
         height: size.height,
-        color: Colors.red,
+        color: myConstants.primaryColor.withOpacity(0.5),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center ,
-            children: [],
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Corrected file path to use forward slashes
+              Image.asset('assets/images/get-started.png'),
+              const SizedBox(
+                height: 30,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Welcome()),
+                  );
+                },
+                child: Container(
+                  height: 50,
+                  width: size.width * 0.7,
+                  decoration: BoxDecoration(
+                    color: myConstants.primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Get started',
+                      style: TextStyle(color: Color.fromARGB(255, 243, 238, 238), fontSize: 18),
+                    ),
+                  )
+                ),
+              ),
+            ],
           ),
-        ) ,
+        ),
       ),
-
-
-      );
-
+    );
   }
 }
